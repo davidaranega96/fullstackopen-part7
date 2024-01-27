008076a8c202810b97f1639f9f1e8a7f78d08ae3
David Aranega <david1996a@gmail.com>
2024-01-27 12:19:30 +0100

    corrected configuration of eslint

--- fullstackopen-part5/.eslintrc.js
+++ fullstackopen-part5/.eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
-    'plugin:prettier/recommended'
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
   ],
   rules: {
     'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
   },
-};
+  plugins: ['react'],
+}
--- fullstackopen-part5/utils/middleware.js
+++ fullstackopen-part5/utils/middleware.js
@@ -1,4 +1,3 @@
-const logger = require('./logger')
 const jwt = require('jsonwebtoken')
 
 const requireAuthentication = {
